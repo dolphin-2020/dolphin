07f5cf7286c72e846094feb342a7d26bc27ed5e6
you <macbookair@Dolphin.local>
2019-12-05 21:03:00 -0600

    remove auto open web-page

--- gulpfile.js
+++ gulpfile.js
@@ -1,70 +1,57 @@
-const{src,dest,parallel,watch}=require("gulp");
-const autoprefix = require("gulp-autoprefixer");
-const clean=require("gulp-clean-css");
-const babel =require("gulp-babel");
-const concat=require("gulp-concat");
-const uglify=require("gulp-uglify");
-const copy=require("gulp-copy");
-const browserSync=require("browser-sync")
-const image=require("gulp-image");
-const sourcemaps=require("gulp-sourcemaps");
+const { src, dest, parallel, watch } = require("gulp");
+const autoprefixer = require("gulp-autoprefixer");
+const clean = require("gulp-clean-css");
+const babel = require("gulp-babel");
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify");
+//const copy=require("gulp-copy");
+const browserSync = require("browser-sync")
+const image = require("gulp-image");
+//const sourcemaps=require("gulp-sourcemaps");
 
-function style(){
+function style() {
   return src("./src/css/**/*.css")
-  .pipe(concat('style.css'))
-  .pipe(autoprefix({
-    browsers:['last 3 versions']
-  }))
-  .pipe(clean())
-  .pipe(dest("./dest/css/"))
+    .pipe(concat('style.css'))
+    .pipe(autoprefixer({
+      browsers: ['last 3 versions']
+    }))
+    .pipe(clean())
+    .pipe(dest("./dest/css/"))
 
 };
 
-function cpImgs(){
+function cpImgs() {
   return src('./src/images/**/*')
-  //.pipe(copy('./'))
-  .pipe(image())
-  .pipe(dest('./dest/images/'))
+    //.pipe(copy('./'))
+    .pipe(image())
+    .pipe(dest('./dest/images/'))
 }
 
-function js(){
-  return src(['./src/js/resources.js','./src/js/app.js','./src/js/engine.js'])
-  //.pipe(sourcemaps.init())
-  .pipe(babel({
-    presets: [
-      ['@babel/preset-env', {modules: false}]
-]
-  }))
-  .pipe(concat('main.js'))
-  .pipe(uglify())
-  //.pipe(sourcemaps.write('.'))
-  .pipe(dest('./dest/js'))
+function js() {
+  return src(['./src/js/resources.js', './src/js/app.js', './src/js/engine.js'])
+    //.pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: [
+        ['@babel/preset-env', { modules: false }]
+      ]
+    }))
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    //.pipe(sourcemaps.write('.'))
+    .pipe(dest('./dest/js'))
 
 }
 
-function cpHtml(){
+function cpHtml() {
   return src('./src/**/*.html')
-  .pipe(dest('./dest/'))
+    .pipe(dest('./dest/'))
 }
 
-function browser(){
-  browserSync.init({
-    server:{
-      baseDir:'./dest/'
-    }
-  })
-}
-
-
-function watcher () {
-  browserSync.init({
-    server: {
-      baseDir: './dest/'
-    }
-  })
+function watcher() {
   watch('./src/css/**/*.scss', style)
-  watch('./src/js/**/*.js',js)
-  watch('src/**/*.html',cpHtml)
+  watch('./src/js/**/*.js', js)
+  watch('src/**/*.html', cpHtml)
+  watch('src/**/*.html', cpImgs)
 }
 
-exports.all=parallel(style,cpHtml,cpImgs,js,watcher);
+exports.all = parallel(style, cpHtml, cpImgs, js, watcher);
